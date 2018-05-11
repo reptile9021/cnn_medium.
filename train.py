9c93cb6d2c3ae58a91c39418662c8f23211b9f04
reptile9021 <38661457+reptile9021@users.noreply.github.com>
2018-05-11 14:40:38 +0800

    Update train.py

--- train.py
+++ train.py
@@ -56,21 +56,21 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
 
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
-training_set = train_datagen.flow_from_directory('C:/Users/JZHU54/PycharmProjects/Classification/training_set',
+training_set = train_datagen.flow_from_directory('yourpath/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory('C:/Users/JZHU54/PycharmProjects/Classification/test_set',
+test_set = test_datagen.flow_from_directory('yourpath/test_set',
                                             target_size = (64, 64),
                                             batch_size = 32,
                                             class_mode = 'binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 800,
-                         epochs = 5,
+                         steps_per_epoch = 8000,
+                         epochs = 25,
                          validation_data = test_set,
-                         validation_steps = 200)
+                         validation_steps = 2000)
 
 
 classifier.save('my_model.h5')  # creates a HDF5 file 'my_model.h5'
